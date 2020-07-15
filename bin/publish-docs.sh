96de0b138222d81d312ce10c53f998879594155b
Nick Tzaperas <nick@nijotz.com>
2020-07-15 16:56:25 -0700

    Added copyright headers to source files

--- bin/publish-docs.sh
+++ bin/publish-docs.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+#
+# Copyright 2020 New Relic Corporation. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 
 VERSION=$(node -e 'console.log(require("./package").version)')
 
