70ffd1398f19a1eeb667b189d4a21b48ecc5a441
Diana Thayer <dthayer@newrelic.com>
2021-12-08 11:10:17 -0800

    add more globber tests

--- tests/unit/versioned/globber.tap.js
+++ tests/unit/versioned/globber.tap.js
@@ -17,56 +17,67 @@ tap.test('globber', (t) => {
   t.test('buildGlobs', (t) => {
     t.autoend()
 
-    t.test('default behavior', (t) => {
-      t.autoend()
+    t.test('expand directory', (t) => {
       const globs = globber.buildGlobs([TEST_DIR])
       t.equal(globs.length, 2)
       t.match(globs, [`${TEST_DIR}/package.json`, `${TEST_DIR}/**/package.json`])
+      t.end()
     })
 
     t.test('glob pattern', (t) => {
-      t.autoend()
-      t.autoend()
       const globs = globber.buildGlobs([`${TEST_DIR}/*.js`])
       t.equal(globs.length, 1)
       t.match(globs, [`${TEST_DIR}/*.js`])
+      t.end()
+    })
+
+    t.test('handle quotes', (t) => {
+      const globs = globber.buildGlobs([`'${TEST_DIR}/*.js'`])
+      t.equal(globs.length, 1)
+      t.match(globs, [`'${TEST_DIR}/*.js'`])
+      t.end()
     })
 
     t.test('specific file', (t) => {
-      t.autoend()
       const globs = globber.buildGlobs([`${TEST_DIR}/other.mock.js`])
       t.equal(globs.length, 1)
       t.match(globs, [`${TEST_DIR}/other.mock.js`])
+      t.end()
     })
   })
 
   t.test('resolveGlobs', (t) => {
     t.autoend()
 
-    t.test('default behavior', async (t) => {
-      t.autoend()
+    t.test('resolve asterisk', async (t) => {
       const files = await globber.resolveGlobs([`${TEST_DIR}/*.js`])
       t.equal(files.length, 2)
       t.match(files, [`${TEST_DIR}/other.mock.js`, `${TEST_DIR}/redis.mock.js`])
+      t.end()
     })
 
-    t.test('filter out node modules', (t) => {
-      t.autoend()
-      // TODO
+    t.test('filter out node modules', async (t) => {
+      const files = await globber.resolveGlobs(
+        [`${TEST_DIR}/**/package.json`],
+        [`${TEST_DIR}/package.json`]
+      )
+      t.equal(files.length, 1)
+      t.match(files, [`${TEST_DIR}/node_modules/@newrelic/package.json`])
+      t.end()
     })
 
     t.test('handle skips', async (t) => {
-      t.autoend()
       const files = await globber.resolveGlobs([`${TEST_DIR}/*.js`], [`${TEST_DIR}/redis.mock.js`])
       t.equal(files.length, 1)
       t.match(files, [`${TEST_DIR}/other.mock.js`])
+      t.end()
     })
 
     t.test('handle duplicates', async (t) => {
-      t.autoend()
       const files = await globber.resolveGlobs([`${TEST_DIR}/*.js`, `${TEST_DIR}/*.js`])
       t.equal(files.length, 2)
       t.match(files, [`${TEST_DIR}/other.mock.js`, `${TEST_DIR}/redis.mock.js`])
+      t.end()
     })
   })
 })
