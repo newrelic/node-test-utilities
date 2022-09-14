8fa5bea6d0f108dff05ee7ceba000d16fc4a9417
Bob Evans <robert.evans25@gmail.com>
2022-09-14 14:42:34 -0400

    added unit tests for the loader logic in lib/versioned/test.js

---
+++ tests/unit/versioned/mock-esm-tests/redis.mock.tap.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+console.log('stdout - redis.mock.tap.js')
+console.error('stderr - redis.mock.tap.js')
+/* eslint-disable no-process-exit */
+process.exit(0)
